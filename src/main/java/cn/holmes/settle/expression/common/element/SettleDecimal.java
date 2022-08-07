0deec467b54d4712087da72b792e412beea1bddb
lynn <lynn@promptness.cn>
2022-08-07 17:17:06 +0800

    rename

--- src/main/java/cn/holmes/settle/expression/common/element/SettleDecimal.java
+++ src/main/java/cn/holmes/settle/expression/common/element/SettleDecimal.java
@@ -9,84 +9,84 @@ import java.util.Objects;
  */
 public class SettleDecimal extends Number implements Comparable<SettleDecimal> {
 
-    private final BigDecimal inner;
+    private final BigDecimal number;
 
     public static SettleDecimal warp(String value) {
         return new SettleDecimal(value);
     }
 
     public BigDecimal bigDecimalValue() {
-        return inner;
+        return number;
     }
 
     public SettleDecimal(String val) {
-        this.inner = new BigDecimal(val);
+        this.number = new BigDecimal(val);
     }
 
     public SettleDecimal(BigDecimal val) {
-        this.inner = val;
+        this.number = val;
     }
 
     /**
      * 除
      */
     public SettleDecimal div(SettleDecimal divisor) {
-        return new SettleDecimal(inner.divide(divisor.inner, 64, RoundingMode.DOWN));
+        return new SettleDecimal(number.divide(divisor.number, 64, RoundingMode.DOWN));
     }
 
     public SettleDecimal div(SettleDecimal divisor, Precision precision) {
-        return new SettleDecimal(inner.divide(divisor.inner, precision.getScale(), precision.getRoundingMode()));
+        return new SettleDecimal(number.divide(divisor.number, precision.getScale(), precision.getRoundingMode()));
     }
 
     /**
      * 乘
      */
     public SettleDecimal mul(SettleDecimal multiplicand, Precision precision) {
-        return new SettleDecimal(inner.multiply(multiplicand.inner).setScale(precision.getScale(), precision.getRoundingMode()));
+        return new SettleDecimal(number.multiply(multiplicand.number).setScale(precision.getScale(), precision.getRoundingMode()));
     }
 
     public SettleDecimal mul(SettleDecimal multiplicand) {
-        return new SettleDecimal(inner.multiply(multiplicand.inner));
+        return new SettleDecimal(number.multiply(multiplicand.number));
     }
 
     /**
      * 减
      */
     public SettleDecimal sub(SettleDecimal subtrahend) {
-        return new SettleDecimal(inner.subtract(subtrahend.inner));
+        return new SettleDecimal(number.subtract(subtrahend.number));
     }
 
     public SettleDecimal sub(SettleDecimal subtrahend, Precision precision) {
-        return new SettleDecimal(inner.subtract(subtrahend.inner).setScale(precision.getScale(), precision.getRoundingMode()));
+        return new SettleDecimal(number.subtract(subtrahend.number).setScale(precision.getScale(), precision.getRoundingMode()));
     }
 
     /**
      * 加
      */
     public SettleDecimal add(SettleDecimal augend) {
-        return new SettleDecimal(inner.add(augend.inner));
+        return new SettleDecimal(number.add(augend.number));
     }
 
     public SettleDecimal add(SettleDecimal augend, Precision precision) {
-        return new SettleDecimal(inner.add(augend.inner).setScale(precision.getScale(), precision.getRoundingMode()));
+        return new SettleDecimal(number.add(augend.number).setScale(precision.getScale(), precision.getRoundingMode()));
     }
 
     /**
      * 幂
      */
     public SettleDecimal pow(SettleDecimal index, Precision precision) {
-        return new SettleDecimal(inner.pow(index.inner.intValue()).setScale(precision.getScale(), precision.getRoundingMode()));
+        return new SettleDecimal(number.pow(index.number.intValue()).setScale(precision.getScale(), precision.getRoundingMode()));
     }
 
     public SettleDecimal pow(SettleDecimal index) {
-        return new SettleDecimal(inner.pow(index.inner.intValue()));
+        return new SettleDecimal(number.pow(index.number.intValue()));
     }
 
     /**
      * 最大
      */
     public SettleDecimal max(SettleDecimal other) {
-        if (inner.compareTo(other.inner) >= 0) {
+        if (number.compareTo(other.number) >= 0) {
             return this;
         } else {
             return other;
@@ -97,7 +97,7 @@ public class SettleDecimal extends Number implements Comparable<SettleDecimal> {
      * 最小
      */
     public SettleDecimal min(SettleDecimal other) {
-        if (inner.compareTo(other.inner) <= 0) {
+        if (number.compareTo(other.number) <= 0) {
             return this;
         } else {
             return other;
@@ -108,51 +108,51 @@ public class SettleDecimal extends Number implements Comparable<SettleDecimal> {
      * 取舍
      */
     public SettleDecimal off(Precision precision) {
-        return new SettleDecimal(inner.setScale(precision.getScale(), precision.getRoundingMode()));
+        return new SettleDecimal(number.setScale(precision.getScale(), precision.getRoundingMode()));
     }
 
     /**
      * 取模
      */
     public SettleDecimal remainder(SettleDecimal divisor) {
-        return new SettleDecimal(inner.remainder(divisor.inner));
+        return new SettleDecimal(number.remainder(divisor.number));
     }
 
     /**
      * 相反数
      */
     public SettleDecimal negate() {
-        return new SettleDecimal(inner.negate());
+        return new SettleDecimal(number.negate());
     }
 
     @Override
     public String toString() {
-        return String.valueOf(inner);
+        return String.valueOf(number);
     }
 
     @Override
     public int compareTo(SettleDecimal o) {
-        return inner.compareTo(o.inner);
+        return number.compareTo(o.number);
     }
 
     @Override
     public int intValue() {
-        return inner.intValue();
+        return number.intValue();
     }
 
     @Override
     public long longValue() {
-        return inner.longValue();
+        return number.longValue();
     }
 
     @Override
     public float floatValue() {
-        return inner.floatValue();
+        return number.floatValue();
     }
 
     @Override
     public double doubleValue() {
-        return inner.doubleValue();
+        return number.doubleValue();
     }
 
     @Override
@@ -164,11 +164,11 @@ public class SettleDecimal extends Number implements Comparable<SettleDecimal> {
             return false;
         }
         SettleDecimal that = (SettleDecimal) o;
-        return Objects.equals(inner, that.inner);
+        return Objects.equals(number, that.number);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(inner);
+        return Objects.hash(number);
     }
 }
