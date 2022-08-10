7d263aa7ac4bd5f763540322267904b0593052bb
lynn <lynn@promptness.cn>
2022-08-10 17:45:07 +0800

    HolmesFunction

--- src/main/java/cn/holmes/settle/expression/common/SpringBean.java
+++ src/main/java/cn/holmes/settle/expression/common/SpringBean.java
@@ -1,12 +1,22 @@
 package cn.holmes.settle.expression.common;
 
+import cn.holmes.settle.expression.common.annotation.HolmesFunction;
+import cn.holmes.settle.expression.lang.opt.custom.CustomMake;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.LinkedList;
+import java.util.List;
 
 @Configuration
-public class SpringBean implements ApplicationContextAware, DisposableBean {
+public class SpringBean implements ApplicationContextAware, DisposableBean, BeanPostProcessor {
 
     private static volatile ApplicationContext shareApplicationContext;
 
@@ -46,5 +56,23 @@ public class SpringBean implements ApplicationContextAware, DisposableBean {
             throw new ElException("applicationContext属性为null,请检查是否注入成功!");
         }
         return shareApplicationContext;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> clazz = bean.getClass();
+        final List<Method> res = new LinkedList<>();
+        ReflectionUtils.doWithMethods(clazz, method -> {
+            if (Modifier.isPublic(method.getModifiers()) && Modifier.isStatic(method.getModifiers())) {
+                if (method.getAnnotation(HolmesFunction.class) != null) {
+                    res.add(method);
+                }
+            }
+        });
+        for (Method method : res) {
+            HolmesFunction holmesFunction = method.getAnnotation(HolmesFunction.class);
+            CustomMake.me().register(holmesFunction.value(), method);
+        }
+        return bean;
     }
 }
